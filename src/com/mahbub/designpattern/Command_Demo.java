33f416477caeb3fccbf86384d65bf2e2343a5fd0
Kazi Mahbubur Rahman <mahbub.kr@samsung.com>
2017-09-08 10:54:14 +0600

    Compound and Composite pattern implemented
    Signed-off-by: Kazi Mahbubur Rahman <mahbub.kr@samsung.com>

--- src/com/mahbub/designpattern/Command_Demo.java
+++ src/com/mahbub/designpattern/Command_Demo.java
@@ -1,4 +1,126 @@
 package com.mahbub.designpattern;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+/**
+ * Encapsulate a request as an object, thereby letting you parameterizeclients with
+ * different requests, queue or log requests, and supportundoable operations.
+ */
+
+
+class Book {
+    private String currentOwner;
+
+    public Book()
+    {
+        this.currentOwner = "Library";
+    }
+
+    public void buy()
+    {
+        System.out.println("Buying the book...");
+        this.currentOwner = "Me";
+    }
+    public void sell()
+    {
+        System.out.println("Selling the book...");
+        this.currentOwner = "Library";
+    }
+    public void giftToFriend(String friendName)
+    {
+        System.out.println("Sending the book as gift to " + friendName);
+        this.currentOwner = friendName;
+    }
+
+    public String getCurrentOwner()
+    {
+        return this.currentOwner;
+    }
+}
+
+interface Command {
+    void execute();
+}
+
+class BuyCommand implements Command {
+    private Book book;
+
+    public BuyCommand(Book book) {
+        this.book = book;
+    }
+
+    @Override
+    public void execute() {
+        book.buy();
+    }
+}
+
+class SellCommand implements Command {
+    private Book book;
+
+    public SellCommand(Book book) {
+        this.book = book;
+    }
+
+    @Override
+    public void execute() {
+        book.sell();
+    }
+}
+
+class GiftFriendCommand implements Command {
+    private Book book;
+    private String friendName;
+
+    public GiftFriendCommand(Book book, String friendName) {
+        this.book = book;
+        this.friendName = friendName;
+    }
+
+    @Override
+    public void execute() {
+        book.giftToFriend(friendName);
+    }
+}
+
+class CommandExecutor {
+    private Queue<Command> nextCommands;
+    private Stack<Command> executedCommands;
+
+    public CommandExecutor()
+    {
+        this.nextCommands = new LinkedList<>();
+        this.executedCommands = new Stack<>();
+    }
+
+    public void addCommand(Command cmd)
+    {
+        this.nextCommands.add(cmd);
+    }
+
+    public void runCommand()
+    {
+        while( ! nextCommands.isEmpty() ) {
+            Command tempCmd = nextCommands.poll();
+            tempCmd.execute();
+            this.executedCommands.add(tempCmd);
+        }
+    }
+}
+
 public class Command_Demo {
+    public static void main(String args[]) {
+        Book book1 = new Book();
+        CommandExecutor executor = new CommandExecutor();
+        executor.addCommand(new BuyCommand(book1));
+        executor.runCommand();
+        System.out.println("Current Owner -- " + book1.getCurrentOwner());
+
+        executor.addCommand(new GiftFriendCommand(book1, "Mahbub"));
+        executor.addCommand(new SellCommand(book1));
+        executor.runCommand();
+        System.out.println("Current Owner -- " + book1.getCurrentOwner());
+    }
 }
--- src/com/mahbub/designpattern/Composite_Demo.java
+++ src/com/mahbub/designpattern/Composite_Demo.java
@@ -1,4 +1,188 @@
 package com.mahbub.designpattern;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Compose objects into tree structures to represent part-whole hierarchies.
+ * Composite lets clients treat individual objects and compositions of objects
+ * uniformly.
+ */
+
+interface AnimalComp {
+    void add(AnimalComp comp);
+    void remove(AnimalComp comp);
+    AnimalComp getChild(int index);
+    void print();
+}
+
+// Leaf Object
+class Head implements AnimalComp {
+    @Override
+    public void add(AnimalComp comp) {
+        // Nothing to add into leaf object
+    }
+
+    @Override
+    public void remove(AnimalComp comp) {
+        // Nothing to remove from leaf object
+    }
+
+    @Override
+    public AnimalComp getChild(int index) {
+        return null;
+    }
+
+    @Override
+    public void print() {
+        System.out.println("Creating Head...");
+    }
+}
+
+// Leaf Object
+class Leg implements AnimalComp {
+    @Override
+    public void add(AnimalComp comp) {
+        // Nothing to add into leaf object
+    }
+
+    @Override
+    public void remove(AnimalComp comp) {
+        // Nothing to remove from leaf object
+    }
+
+    @Override
+    public AnimalComp getChild(int index) {
+        return null;
+    }
+    @Override
+    public void print() {
+        System.out.println("Creating Leg...");
+    }
+}
+
+// Leaf Object
+class Body implements AnimalComp {
+    @Override
+    public void add(AnimalComp comp) {
+        // Nothing to add into leaf object
+    }
+
+    @Override
+    public void remove(AnimalComp comp) {
+        // Nothing to remove from leaf object
+    }
+
+    @Override
+    public AnimalComp getChild(int index) {
+        return null;
+    }
+    @Override
+    public void print() {
+        System.out.println("Creating Body...");
+    }
+}
+
+// Leaf Object
+class Horn implements AnimalComp {
+    @Override
+    public void add(AnimalComp comp) {
+        // Nothing to add into leaf object
+    }
+
+    @Override
+    public void remove(AnimalComp comp) {
+        // Nothing to remove from leaf object
+    }
+
+    @Override
+    public AnimalComp getChild(int index) {
+        return null;
+    }
+    @Override
+    public void print() {
+        System.out.println("Creating Horn...");
+    }
+}
+
+// Leaf Object
+class Tail implements AnimalComp {
+    @Override
+    public void add(AnimalComp comp) {
+        // Nothing to add into leaf object
+    }
+
+    @Override
+    public void remove(AnimalComp comp) {
+        // Nothing to remove from leaf object
+    }
+
+    @Override
+    public AnimalComp getChild(int index) {
+        return null;
+    }
+    @Override
+    public void print() {
+        System.out.println("Creating Tail...");
+    }
+}
+
+class Animal implements AnimalComp {
+    private List<AnimalComp> childAnimalComp = new ArrayList<>();
+
+    public Animal() { }
+
+    @Override
+    public void add(AnimalComp comp) {
+        childAnimalComp.add(comp);
+    }
+
+    @Override
+    public void remove(AnimalComp comp) {
+        childAnimalComp.remove(comp);
+    }
+
+    @Override
+    public AnimalComp getChild(int index) {
+        return childAnimalComp.get(index);
+    }
+
+    @Override
+    public void print() {
+        for(AnimalComp comp : childAnimalComp) comp.print();
+        System.out.println("Composition Completed");
+    }
+}
+
 public class Composite_Demo {
+    public static void main(String args[]) {
+        AnimalComp head = new Head();
+        AnimalComp leg = new Head();
+        AnimalComp horn = new Horn();
+        AnimalComp tail = new Tail();
+        AnimalComp body = new Body();
+
+        // Creating a horse
+        Animal horse = new Animal();
+        horse.add(head);
+        horse.add(leg);
+        horse.add(tail);
+        horse.add(body);
+        horse.print();
+
+        // Creating a Cow
+        Animal cow = new Animal();
+        horse.add(head);
+        horse.add(leg);
+        horse.add(horn);
+        horse.add(tail);
+        horse.add(body);
+        horse.print();
+
+        // Creating a collection of animals
+        Animal animals = new Animal();
+        animals.add(horse);
+        animals.add(cow);
+        animals.print();
+    }
 }
