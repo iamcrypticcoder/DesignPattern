f765deb1feffcfbc5731b5823785d45b5a5287d3
Kazi Mahbubur Rahman <mahbub.kuet@gmail.com>
2017-09-09 01:16:04 +0600

    Facade and Strategy pattern implemented
    Signed-off-by: Kazi Mahbubur Rahman <mahbub.kuet@gmail.com>

--- src/com/mahbub/designpattern/facade/Facade_Demo.java
+++ src/com/mahbub/designpattern/facade/Facade_Demo.java
@@ -1,4 +1,89 @@
 package com.mahbub.designpattern.facade;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Provide a unified interface to a set of interfaces in a subsystem.
+ * Facade defines a higher-level interface that makes the subsystem easier to use.
+ */
+
+// Subsystem
+class PacketSender {
+    public void sendPacket(String packet) {
+        System.out.println(String.format("Packet sent: [%s]", packet));
+    }
+}
+
+class DataSpliter {
+    private int packetSize;
+
+    public DataSpliter(int packetSize) {
+        this.packetSize = packetSize;
+    }
+
+    public List<String> splitData(String data) {
+        int dataLength = data.length();
+        List<String> retList = new ArrayList<>();
+        for(int i = 0; i < dataLength; i += packetSize) {
+            int endIndex = i + packetSize > dataLength ? dataLength : i + packetSize;
+            retList.add(data.substring(i, endIndex));
+        }
+        return retList;
+    }
+}
+
+// Subsystem
+class DataCompressor {
+    private Protocol.CompressionAlgorithm compressionAlgorithm;
+
+    public DataCompressor() {
+    }
+
+    public DataCompressor(Protocol.CompressionAlgorithm compressionAlgorithm) {
+        this.compressionAlgorithm = compressionAlgorithm;
+    }
+
+    public void setCompressionAlgorithm(Protocol.CompressionAlgorithm compressionAlgorithm) {
+        this.compressionAlgorithm = compressionAlgorithm;
+    }
+
+    public String compress(String data) {
+        switch (compressionAlgorithm) {
+            case GZIP:
+                return data;
+            case BZIP:
+                return data;
+            default:
+                return null;
+        }
+    }
+}
+
+// Facade
+class Protocol {
+    enum CompressionAlgorithm { GZIP, BZIP };
+
+    DataCompressor dataCompressor;
+    DataSpliter dataSpliter;
+    PacketSender packetSender;
+
+    public Protocol(CompressionAlgorithm compressionAlgorithm, int packetSize) {
+        dataCompressor = new DataCompressor(compressionAlgorithm);
+        dataSpliter = new DataSpliter(packetSize);
+        packetSender = new PacketSender();
+    }
+
+    public void sendData(String data) {
+        data = dataCompressor.compress(data);
+        List<String> packetList = dataSpliter.splitData(data);
+        for(String s : packetList) packetSender.sendPacket(s);
+    }
+}
+
 public class Facade_Demo {
+    public static void main(String args[]) {
+        Protocol protocol = new Protocol(Protocol.CompressionAlgorithm.GZIP, 5);
+        protocol.sendData("I am a good boy");
+    }
 }
--- src/com/mahbub/designpattern/strategy/Strategy_Demo.java
+++ src/com/mahbub/designpattern/strategy/Strategy_Demo.java
@@ -1,4 +1,78 @@
 package com.mahbub.designpattern.strategy;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Define a family of algorithms, encapsulate each one, and make theminterchangeable.
+ * Strategy lets the algorithm vary independently fromclients that use it.
+ *
+ * AKA: Policy Pattern
+ */
+
+interface PostAnalyzeStrategy {
+    void analyze(List<Post> posts);
+}
+
+class SadnessAnalyzeStrategy implements PostAnalyzeStrategy {
+    @Override
+    public void analyze(List<Post> posts) {
+        int postCount = posts.size();
+        int totalSadCount = 0;
+        for(Post post : posts) totalSadCount += post.sadCount;
+        System.out.println("Sadness Value: " + (double)totalSadCount / postCount);
+    }
+}
+
+class LaughAnalyzeStrategy implements PostAnalyzeStrategy {
+    @Override
+    public void analyze(List<Post> posts) {
+        int postCount = posts.size();
+        int totalLaughCount = 0;
+        for(Post post : posts) totalLaughCount += post.laughCount;
+        System.out.println("Laugh Value: " + (double)totalLaughCount / postCount);
+    }
+}
+
+class Post {
+    String text;
+    int likeCount;
+    int sadCount;
+    int laughCount;
+
+    public Post(String text, int likeCount, int sadCount, int laughCount) {
+        this.text = text;
+        this.likeCount = likeCount;
+        this.sadCount = sadCount;
+        this.laughCount = laughCount;
+    }
+}
+
+class User {
+    List<Post> posts = new ArrayList<>();
+
+    public User() {
+        posts = new ArrayList<>();
+    }
+
+    public void addPost(Post post) {
+        posts.add(post);
+    }
+
+    public void analyzePosts(PostAnalyzeStrategy strategy) {
+        strategy.analyze(posts);
+    }
+}
+
 public class Strategy_Demo {
+    public static void main(String args[]) {
+        User user = new User();
+        user.addPost(new Post("Post 1", 10, 5, 0));
+        user.addPost(new Post("Post 2", 0, 10, 0));
+        user.addPost(new Post("Post 3", 15, 0, 0));
+        user.addPost(new Post("Post 4", 0, 0, 10));
+
+        user.analyzePosts(new SadnessAnalyzeStrategy());
+        user.analyzePosts(new LaughAnalyzeStrategy());
+    }
 }
